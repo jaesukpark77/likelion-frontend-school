6b50bd0b62d16642e7e42a9c24626b5ecfde6c06
Jaeseok Park <jaesukpark77@gmail.com>
2023-02-25 15:14:21 +0900

    Add. Nav 실습 - propTypes 홯용

--- React/likelion-react/stateless-component/vite.config.js
+++ React/likelion-react/stateless-component/vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  // 절대 경로 설정하기
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
   },
   server: {
     host: 'localhost',
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+  css: {
+    devSourcemap: true,
+  },
+});
