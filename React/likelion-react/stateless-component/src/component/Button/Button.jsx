d01c5d18a3bbba89b1c52fba11678f8a109b6815
Jaeseok Park <jaesukpark77@gmail.com>
2023-02-25 13:14:37 +0900

    Add. button 실습 - figma 플러그인을 활용한 theme을 통한 버튼 생성

---
+++ React/likelion-react/stateless-component/src/component/Button/Button.jsx
@@ -0,0 +1,27 @@
+import classes from './Button.module.css';
+import { getColor } from '@/theme/utils';
+
+export function Button({
+  mode,
+  secondary: isSecondary,
+  disabled,
+  ...restProps
+}) {
+  return (
+    <button
+      type="button"
+      className={classes.component}
+      style={{
+        backgroundColor: !isSecondary ? getColor('primary/500') : ('primary/50'),
+        color: !isSecondary ? getColor('white') : getColor('primary/400'),
+      }}
+      {...restProps}
+    />
+  );
+}
+
+Button.defaultProps = {
+  mode: 'primary', // 'secondary'
+  secondary: false,
+  disabled: false,
+};
--- React/likelion-react/stateless-component/vite.config.js
+++ React/likelion-react/stateless-component/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  // 절대 경로 설정하기
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+  },
   server: {
     host: 'localhost',
     port: 3000
